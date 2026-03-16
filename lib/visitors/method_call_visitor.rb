5925bdaf279963605b56acd6c93d81ad3193ebb0
nikitakar9862 <nikitakar9862@gmail.com>
2026-03-16 05:18:56 +0000

    Improve private method detection using class line bounds

--- lib/visitors/method_call_visitor.rb
+++ lib/visitors/method_call_visitor.rb
@@ -14,12 +14,21 @@ module Lowkey
     def visit(node)
       namespace = namespace(node:, parent_map:)
       class_proxy = @file_proxy[namespace]
+      class_proxy.method_calls ||= []
       class_proxy.method_calls << node
 
-      return unless node.name == :private && node.respond_to?(:start_line) && class_proxy.start_line && class_proxy.end_line
-      return unless node.start_line > class_proxy.start_line && node.start_line < class_proxy.end_line
+      return unless node.name == :private 
 
-      class_proxy.private_start_line = node.start_line
+      node_start_line = node.location.start_line
+
+      can_check_bounds = class_proxy.respond_to?(:start_line) && class_proxy.respond_to?(:end_line)
+      can_assign = class_proxy.respond_to?(:private_start_line=)
+       
+      return unless can_check_bounds && can_assign
+
+      if class_proxy.start_line && class_proxy.end_line && node_start_line > class_proxy.start_line && node_start_line < class_proxy.end_line
+        class_proxy.private_start_line = node_start_line
+      end
     end
 
     private
